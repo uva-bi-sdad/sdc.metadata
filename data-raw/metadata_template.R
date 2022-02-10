39ef214c43a5ad4567d6cbc104cfea9759e36059
Joanna Schroeder <js2mr@virginia.edu>
2022-02-10 14:58:42 -0500

    adding doi to sysdata

---
+++ data-raw/metadata_template.R
@@ -0,0 +1,20 @@
+measure_table:measure_name <- list(
+  "category" = "",
+  "measure_table" = "",
+  "full_name" = "",
+  "measure" = "",
+  "type" = "",
+  "short_name" = "",
+  "long_name" = "",
+  "short_description" = "",
+  "long_description" = "",
+  "statement" = "",
+  "sources" = list(
+    list(
+      "name" = "",
+      "date_accessed" = "",
+      "url" = ""
+    )
+  )
+)
+
