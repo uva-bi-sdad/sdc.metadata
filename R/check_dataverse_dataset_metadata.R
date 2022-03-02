50c42130b720b17c946375664856e13dd0c8aba9
Joanna Schroeder <js2mr@virginia.edu>
2022-03-02 17:09:28 -0500

    adding new demographcis

---
+++ R/check_dataverse_dataset_metadata.R
@@ -0,0 +1,12 @@
+# Check dataset metadata
+
+# This will be information for tables, not individual measures
+# Or both?
+
+# First check: On selected measures
+# Unique measure : table_name, does it exist in current metadata? Yes or no
+# If yes, print out information to check if its right
+
+# We can match on expected geographies and geographic units (this is actually in metadata, doesn't need to be provided by user)
+# Match on sources -> create sources
+# Match on citations -> create citations
